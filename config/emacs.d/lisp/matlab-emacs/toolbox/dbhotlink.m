b2751433bbb097d6b58689197255a2539add4413
therockmandolinist <diegoamundo@gmail.com>
2016-09-10 11:01:12 -0400

    Remove matlab-emacs

--- config/emacs.d/lisp/matlab-emacs/toolbox/dbhotlink.m
+++
@@ -1,8 +0,0 @@
-function dbhotlink(L)
-
-   [ST, I] = dbstack('-completenames');
-   if L+1 <=numel(ST)
-      fprintf('<a href="matlab: opentoline(''%s'',%i,1)">%i </a>\n', ST(L+1).file, ...
-              ST(L+1).line, ST(L+1).line);
-   end
-end
