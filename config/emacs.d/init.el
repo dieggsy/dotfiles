c4cb3a5845871d67d633edddf78ebe68a3dd4d26
therockmandolinist <diegoamundo@gmail.com>
2016-10-21 10:07:15 -0400

    Move some variables from init.el into config.org.

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -15,7 +15,6 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
- '(calc-multiplication-has-precedence nil)
  '(compilation-message-face (quote default))
  '(counsel-find-file-ignore-regexp "\\`\\.")
  '(cua-global-mark-cursor-color "#2aa198")
@@ -30,10 +29,8 @@
  '(display-time-format "%a %d %b  %H:%M ")
  '(doc-view-resolution 300)
  '(elfeed-goodies/entry-pane-position (quote bottom))
- '(evil-want-fine-undo t)
  '(fci-rule-color "#3E4451")
  '(fill-column 79)
- '(flycheck-completing-read-function (quote ivy-completing-read))
  '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
  '(highlight-symbol-colors
    (--map
@@ -57,8 +54,6 @@
  '(hl-fg-colors
    (quote
 	("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(markdown-header-scaling t)
- '(muv:completing-read-function (quote ivy-completing-read))
  '(notmuch-hello-sections
    (quote
 	(notmuch-hello-insert-saved-searches notmuch-hello-insert-search notmuch-hello-insert-recent-searches notmuch-hello-insert-alltags notmuch-hello-insert-footer)))
@@ -70,7 +65,6 @@
 	 (:name "sent" :query "tag:sent" :key "t")
 	 (:name "drafts" :query "tag:draft" :key "d")
 	 (:name "all mail" :query "*" :key "a"))))
- '(notmuch-search-oldest-first nil)
  '(nrepl-message-colors
    (quote
 	("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
@@ -82,7 +76,6 @@
 	 (ipython . t))))
  '(org-babel-python-command "python3")
  '(org-confirm-babel-evaluate nil)
- '(org-hide-emphasis-markers t)
  '(org-highlight-latex-and-related nil)
  '(org-latex-classes
    (quote
@@ -114,17 +107,10 @@
  '(org-pandoc-options (quote ((standalone . t))))
  '(package-selected-packages
    (quote
-	(vim-empty-lines-mode matlab matlab-load elfeed-goodies elfeed-org elfeed nm notmuch-labeler focus evil-smartparens coffee-mode evil-org frame-cmds pp-c-l osx-trash evil-magit yapfify smartparens paradox which-key highlight-defined evil-nerd-commenter spray speed-type company-anaconda highlight-quoted pianobar pyenv-mode pyenv multi-eshell selectric-mode highlight-tail fireplace autotetris-mode define-word worf sunshine forecast all-the-icons move-text ace-window virtualenvwrapper eshell-prompt-extras eshell-z material-theme google-translate dropbox palette melpa-upstream-visit helm-dash counsel-osx-app counsel-projectile bongo emms libmpdee landmark zone-select zone-sl zone-rainbow zone-matrix zone-nyan chess eimp e2ansi ranger github-theme org-gcal buffer-stack w3m xkcd notmuch org-beautify-theme 2048-game dired+ restart-emacs devdocs flyspell-correct-ivy flyspell-correct git-gutter-fringe doom-themes tabbar nlinum-relative nlinum google-this eww-lnum w3 emojify git-gutter pbcopy ox-pandoc pdf-tools minesweeper gruvbox-theme anti-zenburn-theme color-theme-sanityinc-tomorrow moe-theme solarized-theme darktooth-theme monokai-theme zenburn-theme multiple-cursors org key-chord yaml-mode yafolding wgrep-ag visual-fill-column vimish-fold vi-tilde-fringe use-package twittering-mode tiny-menu sx sphinx-doc spacemacs-theme spaceline smooth-scroll smex smart-mode-line rainbow-delimiters powerline-evil pacmacs ox-twbs outline-magic origami org-bullets openwith ob-ipython nyan-mode multi-term markdown-preview-mode magit jedi ivy-hydra imenu-anywhere highlight-parentheses highlight-numbers hideshow-org hide-region helm-projectile helm-fuzzier helm-flx helm-dictionary helm-descbinds flycheck fill-column-indicator fancy-battery evil-terminal-cursor-changer evil-leader evil-indent-textobject elpy ein dictionary csv-mode counsel company-jedi avy autopair atom-one-dark-theme apel ag)))
- '(paradox-automatically-star nil)
- '(paradox-execute-asynchronously t)
+	(weechat vim-empty-lines-mode matlab matlab-load elfeed-goodies elfeed-org elfeed nm notmuch-labeler focus evil-smartparens coffee-mode evil-org frame-cmds pp-c-l osx-trash evil-magit yapfify smartparens paradox which-key highlight-defined evil-nerd-commenter spray speed-type company-anaconda highlight-quoted pianobar pyenv-mode pyenv multi-eshell selectric-mode highlight-tail fireplace autotetris-mode define-word worf sunshine forecast all-the-icons move-text ace-window virtualenvwrapper eshell-prompt-extras eshell-z material-theme google-translate dropbox palette melpa-upstream-visit helm-dash counsel-osx-app counsel-projectile bongo emms libmpdee landmark zone-select zone-sl zone-rainbow zone-matrix zone-nyan chess eimp e2ansi ranger github-theme org-gcal buffer-stack w3m xkcd notmuch org-beautify-theme 2048-game dired+ restart-emacs devdocs flyspell-correct-ivy flyspell-correct git-gutter-fringe doom-themes tabbar nlinum-relative nlinum google-this eww-lnum w3 emojify git-gutter pbcopy ox-pandoc pdf-tools minesweeper gruvbox-theme anti-zenburn-theme color-theme-sanityinc-tomorrow moe-theme solarized-theme darktooth-theme monokai-theme zenburn-theme multiple-cursors org key-chord yaml-mode yafolding wgrep-ag visual-fill-column vimish-fold vi-tilde-fringe use-package twittering-mode tiny-menu sx sphinx-doc spacemacs-theme spaceline smooth-scroll smex smart-mode-line rainbow-delimiters powerline-evil pacmacs ox-twbs outline-magic origami org-bullets openwith ob-ipython nyan-mode multi-term markdown-preview-mode magit jedi ivy-hydra imenu-anywhere highlight-parentheses highlight-numbers hideshow-org hide-region helm-projectile helm-fuzzier helm-flx helm-dictionary helm-descbinds flycheck fill-column-indicator fancy-battery evil-terminal-cursor-changer evil-leader evil-indent-textobject elpy ein dictionary csv-mode counsel company-jedi avy autopair atom-one-dark-theme apel ag)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(pos-tip-background-color "#A6E22E")
  '(pos-tip-foreground-color "#272822")
- '(powerline-evil-tag-style (quote verbose))
- '(projectile-globally-ignored-files (quote ("TAGS" ".DS_Store")))
- '(projectile-ignored-projects
-   (quote
-	("/usr/local" "~/dotfiles/config/emacs.d/lisp/Gnugol" "~/dotfiles/config/emacs.d/lisp/matlab-emacs")))
  '(python-shell-completion-native-enable nil)
  '(python-shell-interpreter "python3")
  '(recentf-exclude
