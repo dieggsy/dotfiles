723b7228dc7e3e22dca2a8457ffdc96d182a5042
therockmandolinist <diegoamundo@gmail.com>
2016-10-30 12:09:07 -0400

    Updated darktooth theme.

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -114,7 +114,7 @@
  '(python-shell-interpreter "python3")
  '(recentf-exclude
    (quote
-	("/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|BRANCH_DESCRIPTION\\)\\'" "/elpa/" "/xkcd/")))
+	("/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|BRANCH_DESCRIPTION\\)\\'" "/elpa/" "/xkcd/")) t)
  '(safe-local-variable-values
    (quote
 	((eval when
