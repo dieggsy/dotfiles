b9dddf51b1faa15da87fe4bc8359f2f99d7b8847
therockmandolinist <diegoamundo@gmail.com>
2016-11-03 21:18:04 -0400

    Deal with package initialization less stupidly
    I was going to say "more cleverly," but there's nothing clever about it,
    I was just being weird and hackish before. The new setup should, for
    example, load the latest version of org from the elpa folder instead of
    the preinstalled one.

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -2,7 +2,8 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;; (package-initialize)
+(package-initialize nil)
+(setq package-enable-at-startup nil)
 
 (org-babel-load-file "~/.emacs.d/config.org")
 
