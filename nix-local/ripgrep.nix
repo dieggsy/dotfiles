fab887c69f5c6a5e3b9bc99b5967494b145173df
dieggsy <diegoamundo@protonmail.com>
2017-09-07 16:41:53 -0400

    Update ripgrep.

--- nix-local/ripgrep.nix
+++ nix-local/ripgrep.nix
@@ -9,8 +9,8 @@ buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "BurntSushi";
     repo = "ripgrep";
-    rev = "ca6bd648ab11aeb3b3db9cc570db4d3db52f7bb2";
-    sha256 = "07q50vr4hpghdlq2lm5psa6hyjvcf03hr7jk71hsnjrq98ym5dyj";
+    rev = "beb010d004a79eedb8baa6f4eab21532e83a0ef0";
+    sha256 = "0v23zj4yxky977xvsx9zjc5n0pkclnqjdf58kfniap49v1d22gcs";
   };
 
   depsSha256 = "1g2qm9mdnh79iqwldywa0h81nvxvv3wdpvxns60f4nw9iqz6a7pm";
