2a905a4c3fa0c66373ab1cd34d085230108a51ca
dieggsy <diegoamundo@protonmail.com>
2017-09-13 11:54:32 -0400

    Use xterm-color for finding 256 hex codes.
     - Colors go up to 255, not 256.

--- emacs.d/lisp/term-256color-override.el
+++ emacs.d/lisp/term-256color-override.el
@@ -1,4 +1,5 @@
 (require 'cl-lib)
+(require 'xterm-color)
 
 (defface term-color-light-black
   '((t :foreground "#686868" :background "#686868"))
@@ -40,56 +41,9 @@
   "Face used to render black color code."
   :group 'term)
 
-(defvar term-colors-16-256
-  '("Grey0" "NavyBlue" "DarkBlue" "Blue3" "Blue3" "Blue1" "DarkGreen"
-    "DeepSkyBlue4" "DeepSkyBlue4" "DeepSkyBlue4" "DodgerBlue3" "DodgerBlue2"
-    "Green4" "SpringGreen4" "Turquoise4" "DeepSkyBlue3" "DeepSkyBlue3"
-    "DodgerBlue1" "Green3" "SpringGreen3" "DarkCyan" "LightSeaGreen"
-    "DeepSkyBlue2" "DeepSkyBlue1" "Green3" "SpringGreen3" "SpringGreen2"
-    "Cyan3" "DarkTurquoise" "Turquoise2" "Green1" "SpringGreen2" "SpringGreen1"
-    "MediumSpringGreen" "Cyan2" "Cyan1" "DarkRed" "DeepPink4" "Purple4"
-    "Purple4" "Purple3" "BlueViolet" "Orange4" "Grey37" "MediumPurple4"
-    "SlateBlue3" "SlateBlue3" "RoyalBlue1" "Chartreuse4" "DarkSeaGreen4"
-    "PaleTurquoise4" "SteelBlue" "SteelBlue3" "CornflowerBlue" "Chartreuse3"
-    "DarkSeaGreen4" "CadetBlue" "CadetBlue" "SkyBlue3" "SteelBlue1"
-    "Chartreuse3" "PaleGreen3" "SeaGreen3" "Aquamarine3" "MediumTurquoise"
-    "SteelBlue1" "Chartreuse2" "SeaGreen2" "SeaGreen1" "SeaGreen1"
-    "Aquamarine1" "DarkSlateGray2" "DarkRed" "DeepPink4" "DarkMagenta"
-    "DarkMagenta" "DarkViolet" "Purple" "Orange4" "LightPink4" "Plum4"
-    "MediumPurple3" "MediumPurple3" "SlateBlue1" "Yellow4" "Wheat4" "Grey53"
-    "LightSlateGrey" "MediumPurple" "LightSlateBlue" "Yellow4"
-    "DarkOliveGreen3" "DarkSeaGreen" "LightSkyBlue3" "LightSkyBlue3" "SkyBlue2"
-    "Chartreuse2" "DarkOliveGreen3" "PaleGreen3" "DarkSeaGreen3"
-    "DarkSlateGray3" "SkyBlue1" "Chartreuse1" "LightGreen" "LightGreen"
-    "PaleGreen1" "Aquamarine1" "DarkSlateGray1" "Red3" "DeepPink4"
-    "MediumVioletRed" "Magenta3" "DarkViolet" "Purple" "DarkOrange3"
-    "IndianRed" "HotPink3" "MediumOrchid3" "MediumOrchid" "MediumPurple2"
-    "DarkGoldenrod" "LightSalmon3" "RosyBrown" "Grey63" "MediumPurple2"
-    "MediumPurple1" "Gold3" "DarkKhaki" "NavajoWhite3" "Grey69"
-    "LightSteelBlue3" "LightSteelBlue" "Yellow3" "DarkOliveGreen3"
-    "DarkSeaGreen3" "DarkSeaGreen2" "LightCyan3" "LightSkyBlue1" "GreenYellow"
-    "DarkOliveGreen2" "PaleGreen1" "DarkSeaGreen2" "DarkSeaGreen1"
-    "PaleTurquoise1" "Red3" "DeepPink3" "DeepPink3" "Magenta3" "Magenta3"
-    "Magenta2" "DarkOrange3" "IndianRed" "HotPink3" "HotPink2" "Orchid"
-    "MediumOrchid1" "Orange3" "LightSalmon3" "LightPink3" "Pink3" "Plum3"
-    "Violet" "Gold3" "LightGoldenrod3" "Tan" "MistyRose3" "Thistle3" "Plum2"
-    "Yellow3" "Khaki3" "LightGoldenrod2" "LightYellow3" "Grey84"
-    "LightSteelBlue1" "Yellow2" "DarkOliveGreen1" "DarkOliveGreen1"
-    "DarkSeaGreen1" "Honeydew2" "LightCyan1" "Red1" "DeepPink2" "DeepPink1"
-    "DeepPink1" "Magenta2" "Magenta1" "OrangeRed1" "IndianRed1" "IndianRed1"
-    "HotPink" "HotPink" "MediumOrchid1" "DarkOrange" "Salmon1" "LightCoral"
-    "PaleVioletRed1" "Orchid2" "Orchid1" "Orange1" "SandyBrown" "LightSalmon1"
-    "LightPink1" "Pink1" "Plum1" "Gold1" "LightGoldenrod2" "LightGoldenrod2"
-    "NavajoWhite1" "MistyRose1" "Thistle1" "Yellow1" "LightGoldenrod1" "Khaki1"
-    "Wheat1" "Cornsilk1" "Grey100" "Grey3" "Grey7" "Grey11" "Grey15" "Grey19"
-    "Grey23" "Grey27" "Grey30" "Grey35" "Grey39" "Grey42" "Grey46" "Grey50"
-    "Grey54" "Grey58" "Grey62" "Grey66" "Grey70" "Grey74" "Grey78" "Grey82"
-    "Grey85" "Grey89" "Grey93"))
-
 (defmacro term-256-color-define (number color)
   `(defface ,(intern (concat "term-color-" (number-to-string number)))
-     (let ((hex-color (apply 'color-rgb-to-hex (color-name-to-rgb ,color))))
-       `((t :foreground ,hex-color :background ,hex-color)))
+     '((t :foreground ,color :background ,color))
      (format "Color %s" ,number)))
 
 (setq ansi-term-color-vector
@@ -113,7 +67,7 @@
         term-color-light-white]
        (cl-loop for j = 16 then (+ j 1)
                 while (<= j 255)
-                do (eval `(term-256-color-define ,j (nth (- ,j 16) term-colors-16-256)))
+                do (eval `(term-256-color-define ,j ,(xterm-color--256 j)))
                 collect (intern (concat "term-color-" (number-to-string j))))))
 
 (defvar term-ansi-disable-bold t)
@@ -125,13 +79,13 @@
    ((and (= term-terminal-previous-parameter 5)
          (= term-terminal-previous-parameter-2 38)
          (>= parameter 0)
-         (<= parameter 256))
+         (<= parameter 255))
     (setq term-ansi-current-color (+ parameter 1)))
 
    ((and (= term-terminal-previous-parameter 5)
          (= term-terminal-previous-parameter-2 48)
          (>= parameter 0)
-         (<= parameter 256))
+         (<= parameter 255))
     (setq term-ansi-current-bg-color (+ parameter 1)))
 
    ;; Bold  (terminfo: bold)
