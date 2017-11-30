52e8087793aee72b112f7265cb924da930c1c0bf
dieggsy <diegoamundo@protonmail.com>
2017-11-30 15:59:24 -0500

    Remove xterm-color-names from custom.
    - They were themed in my darktooth fork.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -212,11 +212,7 @@
      (320 . "#2896B5")
      (340 . "#2790C3")
      (360 . "#66D9EF"))))
- '(vc-annotate-very-old-color nil)
- '(xterm-color-names
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
- '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+ '(vc-annotate-very-old-color nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
