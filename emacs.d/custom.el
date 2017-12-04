3005fefbec21128b810dc5c916f33ac49d36da19
dieggsy <diegoamundo@protonmail.com>
2017-12-03 22:58:22 -0500

    Guess I don't need ansi-color-faces-vector in custom anymore.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,8 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
  '(compilation-message-face (quote default))
  '(counsel-find-file-ignore-regexp "\\`\\.")
  '(cua-global-mark-cursor-color "#2aa198")
