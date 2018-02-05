ff9d11fe15ded40ecdfdca2c5b6570dbff8802f3
dieggsy <dieggsy@protonmail.com>
2018-02-04 19:55:28 -0500

    Guix: build manifest from custom-hashes.scm

--- guix/manifest.scm
+++ guix/manifest.scm
@@ -1,6 +1,17 @@
+(use-modules (srfi srfi-88))
+
 (specifications->manifest
- '("emacs-git"
-   "nss-certs"
-   "glibc-locales"
-   "font-dejavu"
-   "termite"))
+ (append
+  (map
+   (lambda (spec) (keyword->string (car spec)))
+   (read (open-input-file
+          (format #f
+                  "~a/dotfiles/guix/custom-hashes.scm"
+                  (getenv "HOME")))))
+  '("font-dejavu"
+    "glibc-locales"
+    "neovim"
+    "nss-certs"
+    "termite"
+    "python-hy"
+    "font-weather-icons")))
