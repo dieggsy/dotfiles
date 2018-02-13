4bb6f03ea8ebb94de1152c0ce9623fe446d2481d
dieggsy <dieggsy@protonmail.com>
2018-02-13 00:53:52 -0500

    Bump emacs, don't track other git packages

--- guix/manifest.scm
+++ guix/manifest.scm
@@ -3,15 +3,15 @@
 (specifications->manifest
  (append
   (map
-   (lambda (spec) (keyword->string (car spec)))
+   (lambda (spec)
+     (keyword->string (car spec)))
    (read (open-input-file
           (format #f
                   "~a/dotfiles/guix/custom-hashes.scm"
                   (getenv "HOME")))))
   '("font-dejavu"
+    "font-sarasa-gothic"
+    "font-symbola"
+    "font-weather-icons"
     "glibc-locales"
-    "neovim"
-    "nss-certs"
-    "termite"
-    "python-hy"
-    "font-weather-icons")))
+    "nss-certs")))
