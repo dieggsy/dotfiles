7c634681055288169810ec2470ff90556fb495b4
dieggsy <dieggsy@protonmail.com>
2018-04-03 01:15:21 -0400

    Bump emacs guix version

--- guix/custom-hashes.scm
+++ guix/custom-hashes.scm
@@ -1,8 +1,8 @@
 ((emacs-git:
    (version . "27.0.50")
-   (rev . "11")
-   (gitsha . "b8c415180895f155b4aaebd9cae65124c95b0c98")
-   (sha256 . "1vy1ysbz5jwhn0zw39r0s0j077qblk0ajpqiygqdm5zi4cjx3y6p")
+   (rev . "31")
+   (gitsha . "e3525385a87c92fdca6b3cf929797a731c688a3f")
+   (sha256 . "0fsbg3mcv8amb4rk7c3fw0hn9g9yhkzgjnvj28s6m15k2mrbdd34")
    (repo . "https://git.savannah.gnu.org/git/emacs.git")
    (uri-format
      .
