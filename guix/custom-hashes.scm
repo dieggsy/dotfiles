3c981d70d0fa93cfafb03bae0556763485357c69
dieggsy <dieggsy@protonmail.com>
2018-06-25 23:53:09 -0400

    Remove guix config
    See previous commit

--- guix/custom-hashes.scm
+++
@@ -1,9 +0,0 @@
-((emacs-git:
-   (version . "27.0.50")
-   (rev . "37")
-   (gitsha . "fa3136e07148ab37fabc52007f531e71e41a1d5a")
-   (sha256 . "1a8jls9a3xcjxx7zj22cm54b6q2mc95y4ibqwaki7xrdw4x4bidf")
-   (repo . "https://git.savannah.gnu.org/git/emacs.git")
-   (uri-format
-     .
-     "https://git.savannah.gnu.org/cgit/emacs.git/snapshot/emacs-~a.tar.gz")))
