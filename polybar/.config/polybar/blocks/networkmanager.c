cba7d20cb0433f93e65c5c5ea233da7a0d9eb9b9
dieggsy <dieggsy@protonmail.com>
2019-04-09 15:26:33 -0400

    Change wifi icon if vpn connected

--- polybar/.config/polybar/blocks/networkmanager.c
+++ polybar/.config/polybar/blocks/networkmanager.c
@@ -9,6 +9,49 @@ int string_has_suffix(const char* suffix, const char* str) {
     return (strncmp(suffix,str + (len - slen), slen) == 0) ? 1 : 0;
 }
 
+int nm_get_active_connections(sd_bus *bus, sd_bus_error *error, sd_bus_message **msg) {
+    return sd_bus_get_property(bus,
+                               "org.freedesktop.NetworkManager",
+                               "/org/freedesktop/NetworkManager",
+                               "org.freedesktop.NetworkManager",
+                               "ActiveConnections", error, msg, "ao");
+}
+
+int nm_vpn_connected(sd_bus *bus, sd_bus_error *error, sd_bus_message **msg) {
+
+    char * path;
+    int r = nm_get_active_connections(bus, error, msg);
+
+    if (r < 0) {
+        // If can't get active connections, just fail silently as if no vpn
+        return 0;
+    }
+    r = sd_bus_message_enter_container(*msg, 'a', "o");
+    while (sd_bus_message_read(*msg, "o", &path) > 0) {
+
+        // Initialize a new message so as to not mes with msg, which we're
+        // using to iterate over the active connections array.
+        sd_bus_message *nmsg = NULL;
+        r = sd_bus_get_property(bus,
+                                "org.freedesktop.NetworkManager",
+                                path,
+                                "org.freedesktop.NetworkManager.Connection.Active",
+                                "Type", error, &nmsg, "s");
+
+        char * type;
+        r = sd_bus_message_read_basic(nmsg, 's', &type);
+
+        // Set icon
+        if (string_has_suffix("vpn", type)
+            || string_has_suffix("tun", type)) {
+            sd_bus_message_exit_container(*msg);
+            return 1;
+        }
+    }
+    sd_bus_message_exit_container(*msg);
+    return 0;
+}
+
 int main () {
 
     int r;
@@ -24,12 +67,10 @@ int main () {
         return r;
     }
 
+    int vpn_connected = nm_vpn_connected(bus, &error, &msg);
     // Get active connections
-    r = sd_bus_get_property(bus,
-                            "org.freedesktop.NetworkManager",
-                            "/org/freedesktop/NetworkManager",
-                            "org.freedesktop.NetworkManager",
-                            "ActiveConnections", &error, &msg, "ao");
+    r = nm_get_active_connections(bus, &error, &msg);
+
     if (r < 0) {
         fputs("Failed to read active connections\n", stderr);
         return r;
@@ -58,7 +99,7 @@ int main () {
         // Set icon
         char *icon;
         if (string_has_suffix("wireless", type)) {
-            icon = "";
+            icon = vpn_connected ? "" : "";
         }
         else if (string_has_suffix("ethernet", type)) {
             icon = "";
