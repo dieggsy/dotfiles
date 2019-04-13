6b03aadf2cc4de0ea0e53376e26f8f1cd142ad86
dieggsy <dieggsy@protonmail.com>
2019-04-13 14:26:26 -0400

    Add low battery notification to battery block

--- polybar/.config/polybar/blocks/battery-average.c
+++ polybar/.config/polybar/blocks/battery-average.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <dirent.h>
 #include <string.h>
+#include <systemd/sd-bus.h>
+#include <espeak-ng/speak_lib.h>
 
 
 int read_int(const char* filename) {
@@ -13,13 +15,6 @@ int read_int(const char* filename) {
 }
 
 int get_joint_percent() {
-    /* float energy0 = read_int("/sys/class/power_supply/BAT0/energy_now"); */
-    /* float maxenergy0 = read_int("/sys/class/power_supply/BAT0/energy_full"); */
-
-    /* float energy1 = read_int("/sys/class/power_supply/BAT1/energy_now"); */
-    /* float maxenergy1 = read_int("/sys/class/power_supply/BAT1/energy_full"); */
-
-    /* return (int)roundf((energy1+energy0) / (maxenergy1+maxenergy0) * 100); */
     char * parent_dir = "/sys/class/power_supply/";
     char * fname_now = "energy_now";
     char * fname_full = "energy_full";
@@ -56,7 +51,55 @@ char * get_icon () {
     return "";
 }
 
+int notify(char *title, char* body) {
+    int r;
+    sd_bus *bus=NULL;
+    sd_bus_message *msg=NULL;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+
+    r = sd_bus_open_user(&bus);
+    if (r < 0 ) {
+        return r;
+    }
+    r = sd_bus_call_method(bus,
+                           "org.freedesktop.Notifications",
+                           "/org/freedesktop/Notifications",
+                           "org.freedesktop.Notifications",
+                           "Notify",
+                           &error, &msg, "susssasa{sv}i",
+                           NULL, 0, NULL, title, body, 0, 0, -1);
+
+    if (r < 0) {
+        return r;
+    }
+    return 0;
+}
+
+void say(char *text) {
+    static int initialized = 0;
+
+    if (!initialized) {
+        espeak_Initialize(AUDIO_OUTPUT_PLAYBACK, 0, NULL, 0);
+
+        espeak_VOICE voice = {0};
+        voice.variant = 2;
+        voice.gender = 2;
+
+        espeak_SetVoiceByProperties(&voice);
+        initialized = 1;
+    }
+
+    espeak_Synth(text, strlen(text) +1, 0, POS_CHARACTER, 0, espeakCHARS_AUTO,
+                 NULL, NULL);
+    espeak_Synchronize();
+}
+
 int main () {
-    printf("%s %d", get_icon(), get_joint_percent());
+    int percent = get_joint_percent();
+    printf("%s %d", get_icon(), percent);
+    if (percent <= 10) {
+        notify("Battery critically low", "Consider charging");
+        say("Battery critically low, consider charging");
+    }
     return 0;
 }
