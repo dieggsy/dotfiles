a41480a1a190beac4b92dc1de417ba32e5bf0149
dieggsy <dieggsy@protonmail.com>
2019-07-16 16:00:24 -0400

    Remove files I won't need on servers

--- etc/networkmanager/NetworkManager/dispatcher.d/10-tzupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-interface=$1
-status=$2
-
-if [[ "$status" = "up" ]] && [[ "$interface" != "tun0" ]] ; then
-    echo "UPDATING TIMEZONE"
-    timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
-fi
