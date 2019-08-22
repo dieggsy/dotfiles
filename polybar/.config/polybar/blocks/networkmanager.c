9cf43100a80b1ad79aa75c0a5d19cd565c79859d
dieggsy <dieggsy@protonmail.com>
2019-08-22 00:27:16 -0400

    Nm module - handle no connection

--- polybar/.config/polybar/blocks/networkmanager.c
+++ polybar/.config/polybar/blocks/networkmanager.c
@@ -20,7 +20,7 @@ int main () {
 
     const GPtrArray *active = nm_client_get_active_connections(client);
     for (uint i = 0; i < active->len; i++) {
-        NMActiveConnection* conn= g_ptr_array_index(active, i);
+        NMActiveConnection* conn = g_ptr_array_index(active, i);
         if (nm_active_connection_get_vpn(conn)
             || strncmp(nm_active_connection_get_connection_type(conn),"tun",3) == 0) {
             vpn_on = 1;
@@ -28,11 +28,13 @@ int main () {
     }
 
     devices = nm_client_get_devices (client);
+    int have_conn = 0;
     for (uint i = 0; i < devices->len; i++) {
         NMDevice *device = g_ptr_array_index (devices, i);
         if (NM_IS_DEVICE_WIFI(device)) {
             NMAccessPoint *ap = nm_device_wifi_get_active_access_point(NM_DEVICE_WIFI(device));
             if (ap != NULL){
+                have_conn = 1;
                 GBytes* ssid = nm_access_point_get_ssid(ap);
                 char * ssid_str = nm_utils_ssid_to_utf8(g_bytes_get_data (ssid, NULL),
                                                         g_bytes_get_size (ssid));
@@ -43,9 +45,13 @@ int main () {
         } else if (NM_IS_DEVICE_ETHERNET(device)) {
             NMActiveConnection *conn = nm_device_get_active_connection(device);
             if (conn != NULL) {
+                have_conn = 1;
                 printf("<-> %s\n", nm_active_connection_get_id(conn));
             }
         }
+    }
+    if (!have_conn || devices->len == 0) {
+        printf("! No Connection");
     }
 
     return 0;
