7c9d1a2736c763aa01c8fb861a0ac5c4b7c1d8d8
dieggsy <dieggsy@protonmail.com>
2019-10-13 12:36:19 -0400

    Clean up 10-tzupdate

--- etc/networkmanager/NetworkManager/dispatcher.d/10-tzupdate.sh
+++ etc/networkmanager/NetworkManager/dispatcher.d/10-tzupdate.sh
@@ -5,11 +5,4 @@ status=$2
 if [[ "$status" = "up" ]] && [[ "$interface" != "tun0" ]] ; then
     echo "UPDATING TIMEZONE"
     /usr/bin/tzupdate
-    # if nmcli -t dev | grep -qs 'wlp3s0:.*:Cthulhu'; then
-    #     echo "CONNECTING TO VPN"
-    #     nmcli con up uuid 0857dd14-47c1-4ff8-a444-94cccaa139a8
-    # fi
-# elif [[ "$status" = "down" ]]; then
-#     echo "DISCONNECTING FROM VPN"
-#     nmcli con down uuid 0857dd14-47c1-4ff8-a444-94cccaa139a8
 fi
