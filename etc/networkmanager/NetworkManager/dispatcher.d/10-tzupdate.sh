231305a1f882207a665dba8c01fc7bf094f993c3
dieggsy <dieggsy@protonmail.com>
2020-12-06 17:46:10 -0500

    Use curl and some website directly for timezone
    - It's pretty much what tzupdate did anyway, no point in another dep

--- etc/networkmanager/NetworkManager/dispatcher.d/10-tzupdate.sh
+++ etc/networkmanager/NetworkManager/dispatcher.d/10-tzupdate.sh
@@ -4,5 +4,5 @@ status=$2
 
 if [[ "$status" = "up" ]] && [[ "$interface" != "tun0" ]] ; then
     echo "UPDATING TIMEZONE"
-    /usr/bin/tzupdate
+    timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
 fi
