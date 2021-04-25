4630a18c8b85ac11e19a045e9034af6ed0e69223
dieggsy <dieggsy@pm.me>
2021-04-25 13:01:33 -0400

    Only omit files if there are normal files present

---
+++ emacs/.emacs.d/var/bookmark-default.el
@@ -0,0 +1,15 @@
+;;;; Emacs Bookmark Format Version 1;;;; -*- coding: utf-8-emacs; mode: lisp-data -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("sxhkdrc"
+ (filename . "~/dotfiles/bspwm/.config/sxhkd/sxhkdrc")
+ (front-context-string . "# -*- mode: conf")
+ (rear-context-string)
+ (position . 1))
+("bspwmrc"
+ (filename . "~/dotfiles/bspwm/.config/bspwm/bspwmrc")
+ (front-context-string . "emacsclient -nc ")
+ (rear-context-string . "RTERM=truecolor ")
+ (position . 2917))
+)
